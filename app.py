d776c86465a9a4854721c4817a01752ca38eeec5
patfee <patrick.feeleus@gmail.com>
2025-10-27 19:31:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -356,4 +356,4 @@ def create_app():
 app, server = create_app()
 
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port=int(os.getenv("PORT", 8050)), debug=True)
+    app.run_server(host="0.0.0.0", port=int(os.getenv("PORT", 3000)), debug=True)
